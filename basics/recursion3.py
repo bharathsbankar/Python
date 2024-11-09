49e0d8cfe768407a05c26d07eae4db3e51aaa2af
bharathsbankar <bharathbanakar954@gmail.com>
2024-11-09 15:34:11 +0530

    included without recursion concepts in recursion3.py

--- basics/recursion3.py
+++ basics/recursion3.py
@@ -1,14 +1,18 @@
 #graphs
 #UN-Directed graph
 #   Given "n" no.. of nodes in a undirected graph which does not have self loops and multi edges,
-#   the no of maximaum edges that can present would be : (n-1)+edges(n-1)
+#   the no of maximaum edges that can present would be : (n-1)+edges(n-1) or (n*(n-1))/2
 #*****************************
 #Directed graph
 #   Given "n" no.. of nodes in a directed graph which does not have self loops and multi edges,
-#   the no of maximaum edges that can present would be : 2*(n-1)+edges(n-1)
+#   the no of maximaum edges that can present would be : 2*(n-1)+edges(n-1) or n*(n-1)
 #*****************************
 
 def maxEdges_UnDirectedGraph(n):
+    #without recursion:
+    #return (n*(n-1))/2
+
+    #with recursion:
     if n==2:
         return 1
     if n<=1:
@@ -17,6 +21,10 @@ def maxEdges_UnDirectedGraph(n):
     return (n-1)+maxEdges_UnDirectedGraph(n-1)
 
 def maxEdges_DirectedGraph(n):
+    #without recursion :
+    #return n*(n-1)
+
+    #with recursion:
     if n==2:
         return 2
     if n<=1:
