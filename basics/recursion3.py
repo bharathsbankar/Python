92001fb71033bc14aa948dc408eb093754094550
bharathsbankar <bharathbanakar954@gmail.com>
2024-11-09 15:02:53 +0530

    changed func.. name in recursion3.py

--- basics/recursion3.py
+++ basics/recursion3.py
@@ -1,13 +1,13 @@
 #graphs
 #Given "n" no.. of nodes in a undirected graph which does not have self loops and multi edges,
 # the no of maximaum edges that can present would be : (n-1)+edges(n-1)
-def edges(n):
+def max_edges(n):
     if n==2:
         return 1
     if n<=1:
         return 0
 
-    return (n-1)+edges(n-1)
+    return (n-1)+max_edges(n-1)
 if __name__=="__main__":
     nodes=int(input("enter the number of nodes : " ))
-    print(edges(nodes))
+    print(max_edges(nodes))
