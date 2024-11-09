7341a79303a7ee7207973b420ec18502361ffd7f
bharathsbankar <bharathbanakar954@gmail.com>
2024-11-09 15:26:31 +0530

    recursion3.py : added func..

--- basics/recursion3.py
+++ basics/recursion3.py
@@ -1,13 +1,29 @@
 #graphs
-#Given "n" no.. of nodes in a undirected graph which does not have self loops and multi edges,
-# the no of maximaum edges that can present would be : (n-1)+edges(n-1)
-def max_edges(n):
+#UN-Directed graph
+#   Given "n" no.. of nodes in a undirected graph which does not have self loops and multi edges,
+#   the no of maximaum edges that can present would be : (n-1)+edges(n-1)
+#*****************************
+#Directed graph
+#   Given "n" no.. of nodes in a directed graph which does not have self loops and multi edges,
+#   the no of maximaum edges that can present would be : 2*(n-1)+edges(n-1)
+#*****************************
+
+def maxEdges_UnDirectedGraph(n):
     if n==2:
         return 1
     if n<=1:
         return 0
 
-    return (n-1)+max_edges(n-1)
+    return (n-1)+maxEdges_UnDirectedGraph(n-1)
+
+def maxEdges_DirectedGraph(n):
+    if n==2:
+        return 2
+    if n<=1:
+        return 0
+
+    return 2*(n-1)+maxEdges_DirectedGraph(n-1)
 if __name__=="__main__":
     nodes=int(input("enter the number of nodes : " ))
-    print(max_edges(nodes))
+    print("undirected graph : ",maxEdges_UnDirectedGraph(nodes))
+    print("directed graph : ",maxEdges_DirectedGraph(nodes))
